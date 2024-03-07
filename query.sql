d9edb4795e84937fe86d76f9933467bf67654b39
Sherly Michaelia <76010363+Sherlymch@users.noreply.github.com>
2024-03-07 13:56:36 +0700

    query.sql

--- query.sql
+++ query.sql
@@ -1,36 +1,18 @@
--- Create new table --
-CREATE TABLE Kimia_farma.kf_analisa AS
-SELECT 
-  ft.transaction_id, 
-  ft.date, 
-  kc.branch_id, 
-  kc.branch_name, 
-  kc.kota, 
-  kc.provinsi, 
-  kc.rating AS rating_cabang, 
-  ft.customer_name, 
-  pd.product_id, 
-  pd.product_name, 
-  pd.price AS actual_price, 
-  ft.discount_percentage,
-  -- create nett_sales --
-    CASE
-      WHEN pd.price <= 50000 THEN 0.1
-      WHEN pd.price <= 100000 THEN 0.15
-      WHEN pd.price <= 300000 THEN 0.20
-      WHEN pd.price <= 500000 THEN 0.25
-      ELSE 0.30
-    END AS presentase_gross_laba,
-    (pd.price-(pd.price * ft.discount_percentage)) AS nett_sales,
-  -- create nett_profit --
-     nett_sales * presentase_gross_laba AS nett_profit,
-  ft.rating AS rating_transaksi, 
-FROM Kimia_farma.kf_final_transaction AS ft
--- joining tables --
-INNER JOIN Kimia_farma.kf_kantor_cabang AS kc
-  ON kc.branch_id = ft.branch_id
-INNER JOIN Kimia_farma.kf_product AS pd
-  ON pd.product_id = ft.product_id
-INNER JOIN Kimia_farma.kf_inventory AS ivt
-  ON ivt.branch_id = ft.branch_id
-;
+create table Kimia_farma.kf_analytics as
+SELECT a.transaction_id, a.date, a.branch_id, a.branch_name, a.kota, a.provinsi, a.rating_cabang, a.customer_name, a.product_id, a.product_name, a.actual_price, a.discount_percentage, a.presentase_gross_laba, a.nett_sales, (a.actual_price * a.presentase_gross_laba) - (a.actual_price-a.nett_sales) nett_profit, a.rating_transaksi
+FROM (
+  SELECT ft.transaction_id, ft.date, kc.branch_id, kc.branch_name, kc.kota, kc.provinsi, kc.rating rating_cabang, ft.customer_name, p.product_id, p.product_name, p.price actual_price, ft.discount_percentage,
+  case when p.price <= 50000 then 0.1
+  when p.price > 50000 and p.price <= 100000 then 0.15
+  when p.price > 100000 and p.price <= 300000 then 0.20
+  when p.price > 300000 and p.price <= 500000 then 0.25
+  when p.price > 500000 then 0.30
+  end presentase_gross_laba,
+  (p.price - (p.price * ft.discount_percentage)) nett_sales,
+  ft.rating rating_transaksi 
+  FROM `Kimia_farma.kf_final_transaction` ft
+  LEFT JOIN `Kimia_farma.kf_kantor_cabang` kc
+    ON ft.branch_id = kc.branch_id
+  LEFT JOIN `Kimia_farma.kf_product` p
+    ON ft.product_id = p.product_id
+) a
